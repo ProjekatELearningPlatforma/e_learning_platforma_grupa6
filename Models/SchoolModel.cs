8e04fc0c10372b185e37de954133cabc0691eb50
mdimitrijevicS60-16 <50595476+mdimitrijevicS60-16@users.noreply.github.com>
2020-05-28 23:27:19 +0200

    Update SchoolModel.cs

--- Models/SchoolModel.cs
+++ Models/SchoolModel.cs
@@ -10,5 +10,28 @@ namespace E_Learning_Platforma.Models
 {
   public class SchoolModel
   {    
+     [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string SchoolId { get; set; }
+
+    [BsonElement("SchoolName")]
+    [Required]
+    public string SchoolName { get; set; }
+
+    [BsonElement("SchoolDescription")]
+    [Required]
+    public string SchoolDescription { get; set; }
+
+    [BsonElement("SchoolCourseLinkOne")]
+    [Required]
+    public string SchoolCourseLinkOne { get; set; }
+
+    [BsonElement("SchoolCourseLinkTwo")]
+    [Required]
+    public string SchoolCourseLinkTwo { get; set; }
+
+    [BsonElement("SchoolCourseLinkThree")]
+    [Required]
+    public string SchoolCourseLinkThree { get; set; }
   }
 }
