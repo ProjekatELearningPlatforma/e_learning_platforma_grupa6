d0c262c3fce7c11054d7c4a19d37ce92d7b3599d
Backo <ckodan@gmail.com>
2020-05-29 20:21:49 +0200

    Update site.js

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,12 @@
 ﻿// Write your Javascript code.
+$(document).ready(function () {
+
+});
+
+$('.card-text.name').click(function () {
+  var cardName = $('.card-text.name');
+  var testIndex = $(this).index('.card-text.name');
+  var clickedIndex = $(cardName).eq(testIndex);
+
+  clickedIndex.siblings().slideToggle();
+});
